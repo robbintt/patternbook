93c876652d146ac0b71fe9e84cff12a29c7ea2cf
Trent Robbins <robbintt@gmail.com>
2018-12-17 01:44:53 -0800

    Added ubuntu playbook for ubuntu 18, added exfat role.

--- instructions/servers.md
+++
@@ -1,8 +0,0 @@
-## Servers
-
-This needs written.
-
-
-### Assumptions for `servers.yml`
-
-- Server: Ubuntu 16.04 Latest Release
--- ubuntu.sh
+++ ubuntu.sh
@@ -2,4 +2,4 @@
 
 # enable on first run to turn sudoers pw off
 # ansible-playbook -i hosts patternbox.yml --ask-sudo-pass --ask-vault-pass --verbose --connection=local
-ansible-playbook -i hosts patternbox.yml --ask-vault-pass --verbose --connection=local
+ansible-playbook -i hosts ubuntu.yml --ask-vault-pass --verbose --connection=local
