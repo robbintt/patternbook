950a1735af136fcb32eb05f4935a80c1d6bc8655
Trent Robbins <robbintt@gmail.com>
2018-12-19 14:25:44 -0800

    Added some roles to new ubuntu 18 playbook, moved reboot to a variable

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # enable on first run to turn sudoers pw off
-# ansible-playbook -i hosts patternbox.yml --ask-sudo-pass --ask-vault-pass --verbose --connection=local
+#ansible-playbook -i hosts ubuntu.yml --ask-sudo-pass --ask-vault-pass --verbose --connection=local
 ansible-playbook -i hosts ubuntu.yml --ask-vault-pass --verbose --connection=local
