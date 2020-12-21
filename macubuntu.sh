027e1305d94d81a1bfc0a56e07868a52be6e821e
Trent Robbins <robbintt@gmail.com>
2020-12-20 18:56:52 -0800

    Fix up some ubuntu roles and add mac/ubuntu playbook

---
+++ macubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# enable on first run to turn sudoers pw off
+#ansible-playbook -i hosts ubuntu.yml --ask-sudo-pass --ask-vault-pass --verbose --connection=local
+ansible-playbook -i hosts macubuntu.yml --ask-vault-pass --verbose --connection=local
